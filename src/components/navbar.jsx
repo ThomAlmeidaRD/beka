e5e3d40197444243e1f1098004f21cf8606b0604
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-06 02:25:10 -0300

    profile img

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
-function Navbar() {
+import profile from './profile.jpg'
 
-    let imgLink = 'https://i.pinimg.com/564x/41/59/8b/41598b79697b7e0fcd6f882a0920cd4f.jpg'
+function Navbar() {
 
     return (
         <>
             <nav>
-                <img src={imgLink} alt="" />
+                <img src={profile} alt="" />
                <section>
                <b>@Beka</b>
                <p id="curso">Publicidade</p>
