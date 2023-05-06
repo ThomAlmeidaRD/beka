3575e5dc24c64286db7999e2f8ee91b9729437e3
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-06 01:44:44 -0300

    Banco de Tarefas com IndexDB usando Dexie

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,81 @@ import Navbar from "./components/navbar"
 import Task from './components/task'
 var color_task = ['🤙🏼', '⏱', '🔥', '💖']
 
+import Dexie from "dexie"
+
+var db = new Dexie("MEUDB")
+
+db.version(1).stores({
+  tarefas: '++id,title,emoji,visible'
+})
+
+var body = document.querySelector('body')
+
+body.onload = function () {
+
+  db.tarefas.toArray().then(function (tarefas) {
+
+    var ul = document.querySelector('ul')
+
+    for (var i = 0; i < tarefas.length; i++) {
+
+      var newItem = document.createElement('li')
+      var newItem_Div = document.createElement('div')
+      var newItem_Title = document.createElement('h3')
+      var newItem_Button = document.createElement('ion-icon')
+      var newItem_span = document.createElement('b')
+      var newItem_ID = document.createElement('p')
+
+      newItem_Button.name = 'checkmark'
+      newItem_span.style.padding = '8px'
+      newItem_span.style.borderRadius = '50%'
+
+      newItem_Title.innerText = tarefas[i].title
+      newItem_span.innerText = tarefas[i].emoji
+      newItem_ID.innerText = tarefas[i].id
+      newItem_Div.classList.add('task-div')
+
+      newItem_Div.appendChild(newItem_span)
+      newItem_Div.appendChild(newItem_Title)
+      newItem_Div.appendChild(newItem_ID)
+      newItem_Div.appendChild(newItem_Button)
+      newItem.appendChild(newItem_Div)
+
+      ul.appendChild(newItem)
+
+      newItem_Button.addEventListener('click', function () {
+
+        var thisDIV = this.parentNode
+        var thisLI = thisDIV.parentNode
+        var thisID = thisDIV.childNodes[2].innerText
+        var integerID = parseInt(thisID)
+
+        db.tarefas.delete(integerID)
+        ul.removeChild(thisLI)
+
+        if (ul.childNodes.length == 1) {
+          msg.style.display = 'flex'
+        }
+
+      })
+
+      if (ul.childNodes.length > 1) {
+        msg.style.display = 'none'
+      }
+
+    }
+
+  })
+
+
+}
+
+function clearDB() {
+  db.tarefas.clear().then(() => {
+    console.log('Tabela LIMPA')
+  })
+}
+
 setInterval(function () {
   var count = document.querySelector('#count')
   var ul = document.querySelector('ul')
@@ -37,6 +112,7 @@ function createTask() {
   var newItem_Title = document.createElement('h3')
   var newItem_Button = document.createElement('ion-icon')
   var newItem_span = document.createElement('b')
+  var newItem_ID = document.createElement('p')
 
 
   newItem_Div.classList.add('task-div')
@@ -56,21 +132,45 @@ function createTask() {
 
   newItem_Div.appendChild(newItem_span)
   newItem_Div.appendChild(newItem_Title)
+  newItem_Div.appendChild(newItem_ID)
   newItem_Div.appendChild(newItem_Button)
   newItem.appendChild(newItem_Div)
 
   newItem_Title.innerText = txtTarefa.value
   ul.appendChild(newItem)
-  msg.style.display = 'none'
-  txtTarefa.value = ''
-  form.style.display = 'none'
+
+
+  // db insert
+  db.tarefas.put({
+    title: txtTarefa.value,
+    emoji: newItem_span.innerText,
+    visible: true
+  })
+
+
+  db.tarefas.toArray().then(function (tarefas) {
+
+    console.log(tarefas.length)
+
+    var lastIndex = tarefas.length - 1
+    if (tarefas.length <= 1) {
+      newItem_ID.innerText = tarefas[0].id
+    } else {
+      newItem_ID.innerText = tarefas[lastIndex].id
+    }
+
+  })
+
 
   newItem_Button.addEventListener('click', function () {
 
-    var taskDIV = this.parentNode
-    var taskLI = taskDIV.parentNode
+    var thisDIV = this.parentNode
+    var thisLI = thisDIV.parentNode
+    var thisID = thisDIV.childNodes[2].innerText
+    var intergerID = parseInt(thisID)
 
-    ul.removeChild(taskLI)
+    db.tarefas.delete(intergerID)
+    ul.removeChild(thisLI)
 
     if (ul.childNodes.length == 1) {
       msg.style.display = 'flex'
@@ -78,6 +178,9 @@ function createTask() {
 
   })
 
+  msg.style.display = 'none'
+  txtTarefa.value = ''
+  form.style.display = 'none'
 
 
 }
